ac571e7b360b010c10bc8a9c0720654301b8429b
jha-Manisha <jhamanisha1106@gmail.com>
2022-05-19 09:23:56 +0530

    main

--- code.js
+++ code.js
@@ -1,27 +1,22 @@
+function sortInSuffle(arr){
+    const n = arr.length;
+
+    for(let i=0;i<n;i+=2){
+        if(i>0 && arr[i-1]>arr[i]){
+            swap(arr,i-1,i);
+        }
+        if(i<n-1 && arr[i]< arr[i+1]){
+            swap(arr,i,i+1);
+        }
+    }
+    return arr;
+}
 function swap(arr,x,y){
     let temp = arr[x];
     arr[x]=arr[y];
     arr[y]=temp;
 }
 
-function sortInWaveForm(arr){
-    const n = arr.length
-
-    for(let i=0;i<n;i+=2){
-
-        // if current even is smaller than prev odd
-        if(i>0 && arr[i-1]>arr[i]){
-            swap(arr,i-1,i)
-        }
-
-        // if current even is smaller than next odd
-        if(i<n-1 && arr[i]< arr[i+1]){
-            swap(arr,i,i+1)
-        }
-    }
-    return arr;
-}
-
-const questionArr = [10,90,2,1,5,23];
-const answerArr = sortInWaveForm(questionArr)
-console.log(answerArr)
+const array = [2, 1, 5, 3, 11, 7];
+const result = sortInSuffle(array);
+console.log(result);
